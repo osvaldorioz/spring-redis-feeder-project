8e80189816714dfb7caca70f43fb5782bfb8bf5c
Osvaldo Ignacio Rios Zambrano <osvaldorioz@yahoo.com.mx>
2024-05-27 16:08:48 -0600

    Update FileUploadController.java

--- src/main/java/inc/hey/feeder/json/redis/FileUploadController.java
+++ src/main/java/inc/hey/feeder/json/redis/FileUploadController.java
@@ -1,72 +1,75 @@
 package inc.hey.feeder.json.redis;
+import java.io.IOException;
+import java.util.stream.Collectors;
 
-import inc.hey.feeder.json.redis.storage.StorageFileNotFoundException;
-import inc.hey.feeder.json.redis.storage.StorageService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
+import inc.hey.feeder.json.redis.storage.StorageFileNotFoundException;
+import inc.hey.feeder.json.redis.storage.StorageService;
 
 @Controller
 public class FileUploadController {
 
-    private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
-    private final StorageService storageService;
+	private final StorageService storageService;
 
-    @Autowired
-    public FileUploadController(StorageService storageService) {
-        this.storageService = storageService;
-    }
+	@Autowired
+	public FileUploadController(StorageService storageService) {
+		this.storageService = storageService;
+	}
 
-    @GetMapping("/")
-    public String listUploadedFiles(Model model) throws IOException {
+	@GetMapping("/")
+	public String listUploadedFiles(Model model) throws IOException {
 
-        model.addAttribute("files", storageService.loadAll().map(
-                        path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                                "serveFile", path.getFileName().toString()).build().toUri().toString())
-                .collect(Collectors.toList()));
+		model.addAttribute("files", storageService.loadAll().map(
+				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+						"serveFile", path.getFileName().toString()).build().toUri().toString())
+				.collect(Collectors.toList()));
 
-        return "uploadForm";
-    }
+		return "uploadForm";
+	}
 
-    @GetMapping("/files/{filename:.+}")
-    @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+	@GetMapping("/files/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-        Resource file = storageService.loadAsResource(filename);
+		Resource file = storageService.loadAsResource(filename);
 
-        if (file == null)
-            return ResponseEntity.notFound().build();
+		if (file == null)
+			return ResponseEntity.notFound().build();
 
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    }
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+	}
 
-    @PostMapping("/")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
+	@PostMapping("/upload")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes) {
 
-        storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "Archivo cargado exitosamente " + file.getOriginalFilename() + "!");
+		storageService.store(file);
+		redirectAttributes.addFlashAttribute("message",
+				"Se subio satisfactoriamente el archivo " + file.getOriginalFilename() + "!");
 
-        return "redirect:/";
-    }
+		return "redirect:/";
+	}
 
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-        return ResponseEntity.notFound().build();
-    }
+	@ExceptionHandler(StorageFileNotFoundException.class)
+	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+		return ResponseEntity.notFound().build();
+	}
 
 }
+
